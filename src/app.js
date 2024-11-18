48583feca0c762831ad8d138007f25780cbd7886
VaalentinoB <valentinoburioni@hotmail.com>
2024-11-18 19:23:55 -0300

    swagger

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ import petsRouter from './routes/pets.router.js';
 import adoptionsRouter from './routes/adoption.router.js';
 import sessionsRouter from './routes/sessions.router.js';
 import mongoose from "mongoose";
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 const app = express();
 const PORT = 8080;
 
@@ -17,13 +19,22 @@ app.use('/api/users',usersRouter);
 app.use('/api/pets',petsRouter);
 app.use('/api/adoptions',adoptionsRouter);
 app.use('/api/sessions',sessionsRouter);
-app.use("/api/mocks", mocksRouter); 
+app.use("/api/mocks", mocksRouter);
 app.listen(PORT,()=>console.log(`Listening on ${PORT}`))
 
+const swaggerOptions = { 
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "API Documentation",
+            description: "Api dedicada a la gestión de mascotas",  
+        }
+    }, 
+    apis: ["./src/docs/**/*.yaml"],
+}
 
-
-
-
+const specs = swaggerJSDoc(swaggerOptions)
+app.use("/apidocs", swaggerUi.serve, swaggerUi.setup(specs));
 mongoose.connect("mongodb+srv://valentinoburioni:vburioni1234@apivb.vym0xct.mongodb.net/Pets",)
     .then(() => {
         console.log('Conectado a la base de datos');
